6e5201ca2b975082377a641c7971f9ffe85de6b9
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2017-08-28 18:45:39 +0300

    Major refactoring. Add support rooms. Also add support Socket.io

---
+++ app/main/events.py
@@ -0,0 +1,24 @@
+from flask import session
+from flask_socketio import emit, join_room, leave_room
+from .. import socketio
+
+
+@socketio.on('joined', namespace='/chat')
+def joined():
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    room = session.get('room')
+    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+
+
+@socketio.on('left', namespace='/chat')
+def left():
+    room = session.get('room')
+    leave_room(room)
+    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
+
