c1f094ff4c2378876e80e9390b914b2d8d28bbf6
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2017-08-31 16:56:02 +0300

    Add events

--- app/main/events.py
+++ app/main/events.py
@@ -15,7 +15,7 @@ def joined(message):
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    emit('message', {'msg': message['msg']}, room=room)
 
 
 @socketio.on('left', namespace='/chat')
@@ -30,3 +30,17 @@ def load_messages(message):
     chat_id = message.get('chat_id', 1)
     room = session.get('room')
     emit('load_messages', {'msg': get_messages_by_chat_id(chat_id)}, room=room)
+
+
+@socketio.on("set_id", namespace='/chat')
+def find_id(user_id):
+    session['name'] = user_id
+    session['room'] = user_id
+    emit('load_messages', {'msg': get_messages_by_chat_id(user_id)}, room=user_id)
+
+
+@socketio.on("set_id_admin", namespace='/chat')
+def find_id_admin(user_id):
+    session['name'] = 1
+    session['room'] = user_id
+    emit('load_messages', {'msg': get_messages_by_chat_id(user_id)}, room=user_id)
