e7b6348e4146b801ed44401d1ed09fbab14f4cdc
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2017-08-31 16:44:36 +0300

    CSRF disabled

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ def create_app(debug=False):
     app = Flask(__name__)
     app.debug = debug
     app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+    app.config['WTF_CSRF_ENABLED'] = False
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
