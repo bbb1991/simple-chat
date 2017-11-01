6505a24a1b73041cd31ce4280841047ff5bf4c85
Bagdat Bimaganbetov <bagdat.bimaganbetov@gmail.com>
2017-11-01 15:59:13 +0300

    Changed structure of project

--- Makefile
+++
@@ -1,9 +0,0 @@
-build clean:
-	virtualenv venv
-	source ./venv/bin/activate
-	pip install -r requirements.txt
-run:
-	gunicorn --worker-class eventlet --bind 0.0.0.0:5000 chat:app
-clean:
-	-deactivate
-	-rm -rf ./venv
--- app/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    room = StringField('Room', validators=[DataRequired()])
-    submit = SubmitField('Enter Chatroom')
